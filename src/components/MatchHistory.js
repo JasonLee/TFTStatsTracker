1f4338ace976908167910b3f6ffa9393944a8b61
Jason Lee <j.lee10@outlook.com>
2020-07-18 21:36:46 -0700

    added api for getting player data and added react router
    new api to pull match data
    
    adding traits
    
    added images for units and color for tier
    
    images for rank/profile now show and match has show more button
    
    added chart
    
    light css work and now has time since in match history

--- src/components/MatchHistory.js
+++ src/components/MatchHistory.js
@@ -1,43 +1,94 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router";
 import axios from 'axios';
+import Match from './Match';
+import './Match.css';
+import { Bar } from 'react-chartjs-2';
 
-export default class MatchHistory extends Component {
+class MatchHistory extends Component {
     constructor(props) {
         super(props);
-
         this.state  = {
-            summonerDetails: []
+            matches: [],
+            graph_data: {}
         };
+    }
+    
+    get_placements() {
+        let placement = [0,0,0,0,0,0,0,0];
+
+        for (const match of this.state.matches) {
+            for (const player of match.info.participants) {
+                if(player.puuid === this.props.puuid) {
+                    placement[player.placement - 1] = placement[player.placement - 1] + 1;
+                    break;
+                }
+            }
+        }
+        return placement;
+    }
+
+    build_graph(new_data) {
+
+        this.setState({
+            graph_data: {
+                labels: ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th'],
+                datasets: [
+                    {
+                        backgroundColor: 'rgba(40, 133, 226, 1)',
+                        borderColor: 'rgba(40, 133, 226, 1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(26, 110, 193, 1)',
+                        hoverBorderColor: 'rgba(26, 110, 193, 1)',
+                        data: new_data
+                    }
+                ]
+            }
+        });
     }
 
     // Runs when component has been added
     componentDidMount() {
-        // GET Request to backend
-        axios.get("/api/challenger")
+        axios.get("/api/matches", {
+            params: {
+                puuid: this.props.puuid
+            }
+        })
         .then(res => {
-            const data = res.data;
-            
-            let player_data = data['entries'];
+            let data = res.data;
 
-            player_data.sort((a,b) => {
-                return b.leaguePoints - a.leaguePoints
+            data.sort((a,b) => {
+                return a.info.game_datetime - b.info.game_datetime;
             })
-
-
-            this.setState({ summonerDetails: player_data });
-        });
-    }
+            
+            this.setState({matches: data});
+            this.build_graph(this.get_placements());
+        })
+    };
 
     // What is actually displayed
     render() {
         return (
+        <div>
+            <div className= "barchart">
+                <Bar
+                    data={this.state.graph_data}
+                    options={{
+                        responsive: true,
+                        maintainAspectRatio: true,
+                    }}
+                />
+            </div>
             <div>
-                <ol className="challenger_players">
-                    {this.state.summonerDetails.map(players => 
-                        <li>{ players.summonerName  + ' - ' + players.leaguePoints}</li>
+                <ol>
+                    {this.state.matches.slice(0).reverse().map(match => 
+                        <Match key={match.metadata.match_id} match_data={match} current_player={this.props.puuid}/>
                     )}
                 </ol>
             </div>
+        </div>
         );
     }
 }
+
+export default withRouter(MatchHistory);
