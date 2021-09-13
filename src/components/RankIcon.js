4b477de65ae279edcfe219ec8831632685999d87
Jason Lee <j.lee10@outlook.com>
2021-09-12 21:50:54 -0700

    Fixed Challenger Icon

--- src/components/RankIcon.js
+++ src/components/RankIcon.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Match.css';
+import challenger from '../Assets/tier-icons/tier-icons-base/challenger.png';
 
 function importAll(r) {
     let images = {};
@@ -12,17 +13,23 @@ const images = importAll(require.context('../Assets/tier-icons/', false, /\.png$
 
 
 export default function RankIcon(props) {
-    
+
     let file_name = props.tier + '_' + props.division + '.png';
     file_name = file_name.toLowerCase();
 
-    console.log(file_name)
-
-    if(props.tier) {
-        return (
-            <img src={images[file_name].default} alt={file_name} className="rank-image"/>
-        );
-      }else {
-        return( <> </>)
-      } 
+    console.log(props.tier  !== "CHALLENGER")
+    if (props.tier) {
+        if (props.tier != "CHALLENGER") {
+            return (
+                <img src={images[file_name].default} alt={file_name} className="rank-image" />
+            );
+        } else {
+            return (
+                <img src={ challenger } alt={file_name} className="rank-image" />
+            )
+            
+        }
+    } else {
+        return (<> </>)
+    }
 }
