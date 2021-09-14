9b9e011e2e1e505602143cb9ec1764820b6cdb23
Jason Lee <j.lee10@outlook.com>
2021-09-13 22:47:21 -0700

    more minimal styling on leaderboard

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -4,6 +4,7 @@ import { withRouter } from "react-router";
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
@@ -42,20 +43,27 @@ class SearchPage extends Component {
     // What is actually displayed
     render() {
         return (
-            <Form>
-                <Form.Group as={Col} xs={6}>
-                    <InputGroup size="lg" onSubmit={this.onSubmit}>
-                        <Form.Control
-                            type="text"
-                            placeholder="Search here.."
-                            onChange={this.handleChange}
-                            onKeyDown={this._handleKeyDown}
-                        />
-                         <InputGroup.Text>
-                                <FontAwesomeIcon icon={faPaperPlane} />
-                        </InputGroup.Text>
-                    </InputGroup>
-                </Form.Group>
+            <Form> 
+                <Row class="align-items-center" style={{
+                    display:"flex",
+                    justifyContent:"center",
+                    alignItems:"Center",
+                    height:"80vh"  
+                }}>
+                    <Col xs={6} >
+                        <InputGroup size="lg" onSubmit={this.onSubmit}>
+                            <Form.Control
+                                type="text"
+                                placeholder="Search here.."
+                                onChange={this.handleChange}
+                                onKeyDown={this._handleKeyDown}
+                            />
+                            <InputGroup.Text>
+                                    <FontAwesomeIcon icon={faPaperPlane} />
+                            </InputGroup.Text>
+                        </InputGroup>
+                    </Col>
+                </Row>
             </Form>
         );
     }
