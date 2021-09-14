bf3a97176454a5acb0c97d25cf6e5d558944eff7
Jason Lee <j.lee10@outlook.com>
2021-09-13 22:47:21 -0700

    more minimal styling on leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom"
 import axios from 'axios';
+import Table from 'react-bootstrap/Table';
 
 class LeaderBoard extends Component {
     constructor(props) {
         super(props);
 
-        this.state  = {
+        this.state = {
             summonerDetails: []
         };
     }
@@ -16,36 +17,49 @@ class LeaderBoard extends Component {
     componentDidMount() {
         // GET Request to backend
         axios.get("/api/challenger")
-        .then(res => {
-            const data = res.data;
-            
-            let player_data = data['entries'];
+            .then(res => {
+                const data = res.data;
 
-            player_data.sort((a,b) => {
-                return b.leaguePoints - a.leaguePoints
-            })
+                let player_data = data['entries'];
 
-            this.setState({ summonerDetails: player_data });
-        });
+                player_data.sort((a, b) => {
+                    return b.leaguePoints - a.leaguePoints
+                })
+
+                this.setState({ summonerDetails: player_data });
+            });
     }
 
     // What is actually displayed
     render() {
         return (
-            <div className="centered">
-                <ol className="challenger_players">
-                    {this.state.summonerDetails.map(players => 
-                        <li key={players.summonerId}> 
-                            <Link to={{
+            <Table bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Player</th>
+                        <th>Rank</th>
+                        <th>Points</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {this.state.summonerDetails.map((players, index) =>
+                        <tr key={players.summonerId}>
+                            <td>{index + 1}</td>
+                            <td><Link to={{
                                 pathname: '/player',
                                 search: '?name=' + players.summonerName,
-                                state: {summonerName: players.summonerName}
-
-                            }}>{players.summonerName}</Link> {' - ' + players.leaguePoints}
-                        </li>
+                                state: { summonerName: players.summonerName }
+                            }}>
+                                {players.summonerName}
+                            </Link></td>
+                            <td>{"Challenger " + players.rank}</td>
+                            <td>{players.leaguePoints}</td>
+                        </tr>
                     )}
-                </ol>
-            </div>
+                </tbody>
+            </Table>
         );
     }
 }
