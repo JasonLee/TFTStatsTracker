a3aa2d65e25704ce94822ccbe041798099e58dc2
Jason Lee <j.lee10@outlook.com>
2021-09-16 22:28:55 -0700

    item images shown

--- src/components/Unit.js
+++ src/components/Unit.js
@@ -9,10 +9,13 @@ const importAll = require =>
 
 
 const images = importAll(require.context('../Assets/champions/', true, /\.png$/));
+const itemImages = importAll(require.context('../Assets/items/', true, /\.png$/));
 
 
 // NOTE: TRAITS API REQUEST HAS A BUG - https://github.com/RiotGames/developer-relations/issues/330
 export default function Units(props) {
+    const items = props.unit.items;
+    console.log(items)
     const unit = props.unit.character_id;
 
     let unit_color;
@@ -36,10 +39,20 @@ export default function Units(props) {
             unit_color = "gray"
       }
 
+    const itemImg = items.map((itemID) => {
+        const imgPath = itemID + ".png";
+        const imgSrc = itemImages[imgPath].default;
+
+        return <img src={ imgSrc } style={{ width: 32 }} />
+
+    });
+
     return (
             <div style={{padding:5}}>
-                <img src={images[unit + '.png'].default} alt={unit} style={{ borderColor: unit_color, width: 64}} border="4" /><br/>
+                <img src={images[unit + '.png'].default} alt={unit} style={{ borderColor: unit_color, width: 64}} border="4" />
                 ({props.unit.tier})
+                <br/>
+                { itemImg }
             </div>
     );
 }
